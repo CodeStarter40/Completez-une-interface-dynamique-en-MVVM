74b3ee3a863b3ecc0b2b8cd9391ba7a701927ee9
CodeStarter40 <bhgzpro@pm.me>
2024-01-16 16:26:24 +0100

    ajustements reviewfragment add back button

--- app/src/main/java/com/openclassrooms/tajmahal/ui/restaurant/ReviewFragment.java
+++ app/src/main/java/com/openclassrooms/tajmahal/ui/restaurant/ReviewFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -25,9 +26,19 @@ public class ReviewFragment extends Fragment {
         // Inflate the layout pour ce fragment
         View view = inflater.inflate(R.layout.fragment_review, container, false);
         initRecyclerView(view);
+        setupBackButton(view);
         return view;
     }
 
+    private void setupBackButton(View view) {
+        ImageView backButton = view.findViewById(R.id.backButton);
+        backButton.setOnClickListener(v -> {
+            //logique retour arriere
+            if (getActivity() != null) {
+                getActivity().onBackPressed();
+            }
+        });
+    }
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
